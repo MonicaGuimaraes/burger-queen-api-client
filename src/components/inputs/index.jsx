76ef554e5885f1908863c305d820718a01a09217
Karina Mel <92555432+KarinaMel0@users.noreply.github.com>
2022-05-13 18:01:41 -0300

    feat:Criando a pagina de Login Co-authored-by: MonicaGuimaraes <monicapeixoto0@gmail.com>

--- src/components/inputs/index.jsx
+++ src/components/inputs/index.jsx
@@ -1,12 +1,10 @@
-export default function Inputs(tipoInput,placeholder) {
+export default function Inputs({typeInput,placeholder,value}) {
     return (
       <> 
-      <input className='input'type={tipoInput} placeholder={placeholder} id={placeholder}></input>
+        <input className='input'type={typeInput} placeholder={placeholder} id={placeholder} ></input>
       </>
-        
- 
     );
   }
-
+// sugestão: colocar o input de email e senha dentro do inputs()
 // Como usar parametros nas funções de react
 // Só é necessário criar um input ? E chamar ele passando argumentos para diferenciar
---
+++ src/testeAPI.js
@@ -0,0 +1,14 @@
+export default async function testeAPI(){
+  const options = {
+      method: 'POST',
+      Headers: { 
+        'Content-Type': 'application/json', 
+      },
+      body:{email:'sample@mail.com', password:'sample'},
+    }; 
+    //Bad request no console.log
+    fetch('https://lab-api-bq.herokuapp.com/auth', options)
+     .then((response) => response.json())
+     .then((response) => console.log(response))
+     .catch((err) => console.log(err));
+}
