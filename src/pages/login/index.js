10e895fd683228bf78aff209b42c029300be19b2
MonicaGuimaraes <monicapeixoto0@gmail.com>
2022-05-18 20:30:34 -0300

    att

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,23 +1,26 @@
 import Inputs from "../../components/inputs" 
 import ButtonSubmit from "../../components/buttons"
-// import testeAPI from "../../testeAPI.js"
+import testeAPI from "../../testeAPI.js"
+import { useState } from "react"
 
 export default function Login(){
-  // function onSubmitForm(e) {
-  //   e.preventDefault()
-  //   testeAPI()
-  // }
-  return(
-    <>
-      <form action="https://lab-api-bq.herokuapp.com/auth" method="POST" encType="application/x-www-form-urlencoded">
-        <label> 
-          <h1>Login</h1>
-          <Inputs tipoInput='email' placeholder='email' />
-          <Inputs tipoInput='password' placeholder='senha'/>
-          <ButtonSubmit action={'Entrar'}/>
-        </label>
-      </form>
-    </>    
-  )
-}
-// onSubmit={onSubmitForm}
+   const [value,setValue] = useState()
+    function onSubmitForm(e) {
+      e.preventDefault()
+      testeAPI()
+      console.log(value)
+    }
+    
+    return(
+      <>
+        <form onSubmit={onSubmitForm} >
+          <label> 
+            <h1>Login</h1>
+            <Inputs tipoInput='email' placeholder='email'/>
+            <Inputs tipoInput='password' placeholder='senha' value = {setValue} />
+            <ButtonSubmit action={'Entrar'}/>
+          </label>
+        </form>
+     </>    
+    )
+   }
