667f243b8018d02ffff22a1300bf99a0dc532e52
Mônica Peixoto Guimarães <97262052+MonicaGuimaraes@users.noreply.github.com>
2022-05-30 22:46:56 -0300

    Update index.jsx

--- src/components/buttonsNavigate/index.jsx
+++ src/components/buttonsNavigate/index.jsx
@@ -1,4 +1,4 @@
-import styles from './ButtonNav.module.css'
+import styles from './buttonNav.module.css'
 export default function buttonNav({disable, children, onClick}){
    return(
      <button disabled={disable} onClick={onClick} className={styles.buttonNav}>{children}</button>
